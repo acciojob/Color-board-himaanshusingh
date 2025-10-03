997818136b0414c5f49d5bfdd2ca93b7019deb54
Himanshu Singh <himanshusingh1k@gmail.com>
2025-10-03 11:18:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 //your JS code here. If required.
-const square = document.querySelector(".square");
-square.style.backgroundColor = rgb(29, 29, 29);
+const randomColor = () => {
+  const red = Math.floor(Math.random() * 255) + 1;
+  const green = Math.floor(Math.random() * 255) + 1;
+  const blue = Math.floor(Math.random() * 255) + 1;
+  return `rgb(${red}, ${green}, ${blue})`;
+};
+const allSquares = document.querySelectorAll(".square");
+Array.from(allSquares).forEach((square) => {
+  square.addEventListener("mouseover", function () {
+    this.style.backgroundColor = randomColor();
+    this.style.cursor = "pointer";
+  });
+  square.addEventListener("mouseout", function () {
+    this.style.backgroundColor = "white";
+  });
+});
