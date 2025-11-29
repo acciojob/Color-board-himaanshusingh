081ab38ddb76a0e0ed40ca1f3d57af2fbd4fb4fa
Himanshu Singh <himanshusingh1k@gmail.com>
2025-11-29 17:19:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,31 @@
-//your JS code here. If required.
-const randomColor = () => {
-  const red = Math.floor(Math.random() * 255) + 1;
-  const green = Math.floor(Math.random() * 255) + 1;
-  const blue = Math.floor(Math.random() * 255) + 1;
-  return `rgb(${red}, ${green}, ${blue})`;
-};
-const allSquares = document.querySelectorAll(".square");
-allSquares.forEach((square) => {
-  square.addEventListener("mouseover", () => {
-    square.style.backgroundColor = randomColor();
-    square.style.cursor = "pointer";
+const container = document.querySelector(".container");
+const boxes = document.getElementsByClassName("box");
+
+for (let i = 0; i < 800; i++) {
+  const box = document.createElement("div");
+  box.className = "box";
+  container.append(box);
+}
+
+Array.from(boxes).forEach((box) => {
+  box.addEventListener("mouseover", () => {
+    box.style.backgroundColor = genColor();
   });
-  square.addEventListener("mouseout", () => {
-    square.style.backgroundColor = "rgb(29, 29, 29)";
+  box.addEventListener("mouseout", () => {
+    box.removeAttribute("style");
   });
 });
+
+function randomColor() {
+  return Math.floor(Math.random() * 16).toString(16);
+}
+
+function genColor() {
+  const a = randomColor();
+  const b = randomColor();
+  const c = randomColor();
+  const d = randomColor();
+  const e = randomColor();
+  const f = randomColor();
+  return `#${a}${b}${c}${d}${e}${f}`;
+}
